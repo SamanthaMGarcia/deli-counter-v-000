5e9e73f6f878ee04c85316757ff8b4631fefbdde
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-09 13:58:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,3 +8,8 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+now_serving(katz_deli, name)
+  if katz_deli.length == 0
+   puts "There is nobody waiting to be served!"
+ else 
