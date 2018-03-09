2e1e0e6865a8857c732d90d909ce0525a171a54d
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-09 14:05:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
 katz_deli = []
 
 def line(katz_deli)
+  if katz_deli.length == 0
   puts "The line is currently empty."
+  elsif
 end
 
 def take_a_number(katz_deli, name)
@@ -16,3 +18,19 @@ now_serving(katz_deli, name)
    puts "Currently serving #{name}."
   end
 end
+
+
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return ("The line is currently empty.")
+  } else {
+    var array = []
+     for (let i = 0; i < katzDeliLine.length ;i++) {
+       // `${i + 1}. ${katzDeliLine[i]}` == "1. Grace"
+      array.push(`${i + 1}. ${katzDeliLine[i]}`)
+     }
+    //array ['1. Ada', '2. Grace', '3. Morty']
+     var string = array.join(', ')
+     // string '1. Ada, 2. Grace'
+    return "The line is currently: " + string
