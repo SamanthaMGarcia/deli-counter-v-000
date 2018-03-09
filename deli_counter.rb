ff5d898b39cae0eb2a19e6fa38252b0856e6aa44
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-09 14:57:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-  puts katz_deli.each_with_index { |name, index_number| } 
+  katz_deli.each_with_index { |name, index_number| } 
   puts "The line is currently: #{index_number + 1}. #{name}."
   end
 end
